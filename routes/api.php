99e07c7508ff7396b38e0560b8c52cb5198eebe9
fecsaba <nexusegyesulet@gmail.com>
2020-04-23 22:56:23 +0200

    User list with token

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('users', 'UserController@users');
 });
 Route::post('login', 'UserController@index');
-Route::get('users', 'UserController@users');
+
